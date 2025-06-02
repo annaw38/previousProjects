b43678f74954956a022022a2e211e243fbe25b4a
annaw38 <57265383+annaw38@users.noreply.github.com>
2025-06-02 17:36:27 -0500

    Add files via upload

---
+++ CS400: Programming III/A03: First Git/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("Why did two foos walks into a bar?");
+	System.out.println("To say "+ "\""+ "Hello World"+"\"" +" to their friend Baz.");
+    }
+}
